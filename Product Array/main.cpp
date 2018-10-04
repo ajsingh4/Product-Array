079c6c350a8bd2fbd32e550eff4adccd2d5c792c
Aj Gill <ajgill@ucdavis.edu>
2018-10-04 08:58:50 -0700

    Initial Commit

---
+++ Product Array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Product Array
+//
+//  Created by Aj Gill on 10/4/18.
+//  Copyright © 2018 Aj Gill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
