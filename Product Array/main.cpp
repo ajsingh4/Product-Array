ca2e583e3fe48ebdbdee24f4f24fbdacc3ac1fd6
Aj Gill <ajgill@ucdavis.edu>
2018-10-04 11:18:10 -0700

    Created a function to handle the work
    A bit more modular

--- Product Array/main.cpp
+++ Product Array/main.cpp
@@ -8,16 +8,8 @@
 
 #include <iostream>
 
-int main() {
-    std::cout << "Please enter the size of the array\n";
-    int size;
-    std::cin >> size;
-    int arr[size], it = 0, productArr[size], product = 1, zeroCounter = 0, initialZeroCounter = 0;
-    std::cout << "Please enter the numbers of the array seperated by spaces\n";
-    while(it < size){
-        std::cin >> arr[it];
-        it++;
-    }
+void ProductArr(int* arr, int size){
+    int product = 1, zeroCounter = 0, initialZeroCounter = 0;
     for (int i=0; i<size; i++) {
         if(arr[i] == 0){
             initialZeroCounter ++;
@@ -29,20 +21,34 @@ int main() {
     }
     for (int j=0; j<size; j++) {
         if(arr[j] == 0 && zeroCounter == 0 && initialZeroCounter == 1){
-            productArr[j] = product;
+            arr[j] = product;
             zeroCounter++;
         }
         else if(arr[j] == 0 || zeroCounter > 0 || initialZeroCounter > 1){
-            productArr[j] = 0;
+            arr[j] = 0;
             zeroCounter++;
         }
         else{
-            productArr[j] = product/arr[j];
+            arr[j] = product/ arr[j];
         }
     }
+}
+
+int main() {
+    std::cout << "Please enter the size of the array\n";
+    int size;
+    std::cin >> size;
+    int arr[size], it = 0;
+    std::cout << "Please enter the numbers of the array seperated by spaces\n";
+    while(it < size){
+        std::cin >> arr[it];
+        it++;
+    }
+    ProductArr(arr, size);
+    
     std::cout << "\n";
     for (int z=0; z<size; z++) {
-        std::cout << productArr[z] << " ";
+        std::cout << arr[z] << " ";
     }
     std::cout << "\n";
     
